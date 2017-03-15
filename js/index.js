793c575a1f8a8909a16f17e945d50dcb267c26bf
soul33 <2501459745@qq.com>
2017-03-15 12:22:08 +0800

    css3修改

--- js/index.js
+++ js/index.js
@@ -8,9 +8,10 @@ $(function(){
         //     n=0;
         // }
         $('#fk img').eq(n).fadeIn(500).siblings('img').fadeOut(500);
-        $('#fk ul li').eq(n).css({'background':'#a10000'}).siblings('li').css({'background':'#000'});
+        //$('#fk ul li').eq(n).css({'background':'#a10000'}).siblings('li').css({'background':'#000'});
+        $('#fk ul li').eq(n).addClass('li_run').siblings('li').removeClass('li_run');
     }
-    var timer=setInterval(run,2000);
+    var timer=setInterval(run,3000);
 
     //鼠标移入切换
     $('#fk ul li').mouseenter(function(){
@@ -21,12 +22,13 @@ $(function(){
         timeout = setTimeout(function(){
             n=nthis.index();
             $('#fk img').eq(n).stop().fadeIn(500).siblings('img').stop().fadeOut(500);
-            $('#fk ul li').eq(n).css({'background':'#a10000'}).siblings('li').css({'background':'#000'});
+            //$('#fk ul li').eq(n).css({'background':'#a10000'}).siblings('li').css({'background':'#000'});
+            $('#fk ul li').eq(n).addClass('li_run').siblings('li').removeClass('li_run');
         },200);     
     });
     //鼠标移出停止切换，转回定时器
     $('#fk ul li').mouseleave(function(){
         clearTimeout(timeout);
-        timer=setInterval(run,2000);
+        timer=setInterval(run,3000);
     });
 });
